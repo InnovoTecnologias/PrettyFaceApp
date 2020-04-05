d5215a0d4b0a030c47e011f605f5e99ef3867ef1
Ignacio - Innovo <ignacioelfi@gmail.com>
2020-04-05 08:43:52 -0300

    GitHub´s repository link updated.

--- PrettyFaceApp/PrettyFaceApp/ViewModels/AboutViewModel.cs
+++ PrettyFaceApp/PrettyFaceApp/ViewModels/AboutViewModel.cs
@@ -10,7 +10,7 @@ namespace PrettyFaceApp.ViewModels
         public AboutViewModel()
         {
             Title = "About";
-            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://github.com/ignacioinnovo/PrettyFaceApp"));
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://github.com/InnovoTecnologias/PrettyFaceApp"));
         }
 
         public ICommand OpenWebCommand { get; }
